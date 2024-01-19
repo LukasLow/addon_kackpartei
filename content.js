b9d1f243c57eb48460cf4ed4f4cb6d5b0ddb4462
Lukas <99747166+LukasLow@users.noreply.github.com>
2024-01-19 18:13:05 +0100

    init

---
+++ content.js
@@ -0,0 +1,46 @@
+function replaceText(node) {
+    if (node.nodeType === 3) {
+        let text = node.nodeValue;
+        let replacedText = text.replace(/afd/gi, 'müllpartei');
+        if (replacedText !== text) {
+            node.nodeValue = replacedText;
+        }
+    }
+}
+
+function replaceTextOnPage() {
+    const elements = document.getElementsByTagName('*');
+
+    for (let element of elements) {
+        for (let node of element.childNodes) {
+            if (node.nodeType === 3) {
+                let text = node.nodeValue;
+                let replacedText = text.replace(/afd/gi, 'müllpartei');
+
+                if (replacedText !== text) {
+                    element.replaceChild(document.createTextNode(replacedText), node);
+                }
+            }
+        }
+    }
+}
+
+// Setzt das Intervall für die Funktion
+setInterval(replaceTextOnPage, 1000); // 3000 Millisekunden = 3 Sekunden
+
+
+// Beobachter-Initialisierung
+const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+        mutation.addedNodes.forEach(replaceText);
+    });
+});
+
+// Beobachtungskonfiguration
+const config = { childList: true, subtree: true };
+
+// Starten der Beobachtung
+observer.observe(document.body, config);
+
+// Ersetzen des Textes beim Laden der Seite
+window.onload = replaceTextOnPage;
---
+++ readme.md
@@ -0,0 +1,3 @@
+icon is made by flaticon.com https://www.flaticon.com/free-icon/poop_1642901?term=poop&page=1&position=7&origin=tag&related_id=1642901
+
+<a href="https://www.flaticon.com/free-icons/poop" title="poop icons">Poop icons created by Freepik - Flaticon</a>
